242bb7d4abd1474c38128ffb98cd6b4cd2f59cbe
hrjp <r115159@g.kobe-kosen.ac.jp>
2023-06-13 10:32:36 +0000

    add joy to value

--- src/joy_to_value.cpp
+++ src/joy_to_value.cpp
@@ -1,6 +1,6 @@
 /**
- * @file arm_sim.cpp
- * @brief arm sim
+ * @file joy_to_value.cpp
+ * @brief
  * @author Shunya Hara
  * @date 2023.6.13
  * @details
@@ -10,12 +10,16 @@
 #include <std_msgs/Float32.h>
 #include <geometry_msgs/PoseArray.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <sensor_msgs/Joy.h>
+
+ros::Publisher angle_pub;
+ros::Publisher linear_vel_pub;
 
 void joyCallback(const sensor_msgs::Joy::ConstPtr &msg)
 {
     std_msgs::Float32 angle_msg;
     std_msgs::Float32 linear_vel_msg;
-    angle_msg.data = msg->axes.at(3) *;
+    angle_msg.data = msg->axes.at(3) * 0.5;
     linear_vel_msg.data = msg->axes.at(1) * 0.05;
     angle_pub.publish(angle_msg);
     linear_vel_pub.publish(linear_vel_msg);
@@ -23,17 +27,14 @@ void joyCallback(const sensor_msgs::Joy::ConstPtr &msg)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "wp_loader_node");
+    ros::init(argc, argv, "joy_to_value_node");
     ros::NodeHandle n;
-    // 制御周期10Hz
-    ros::Rate loop_rate(10);
-
     // param setting
     ros::NodeHandle pn("~");
 
     // Publisher
-    ros::Publisher angle_pub = n.advertise<std_msgs::Float32>("angle", 1);
-    ros::PUblisher linear_vel_pub = n.advertise<std_msgs::Float32>("linear_vel", 1);
+    angle_pub = n.advertise<std_msgs::Float32>("angle", 1);
+    linear_vel_pub = n.advertise<std_msgs::Float32>("linear_vel", 1);
     // subscriber
     ros::Subscriber joy_sub = n.subscribe("joy", 1, joyCallback);
 
