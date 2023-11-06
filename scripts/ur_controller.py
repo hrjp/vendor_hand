0f98545ddf13819bc1a3e5627a27e9c000ce1c41
hrjp <r115159@g.kobe-kosen.ac.jp>
2023-11-06 07:30:18 +0000

    Update moveL speed in ur_controller_node

--- scripts/ur_controller.py
+++ scripts/ur_controller.py
@@ -6,16 +6,6 @@ import rtde_receive
 import rtde_control
 from geometry_msgs.msg import Pose
 
-# rtde_c = rtde_control.RTDEControlInterface("192.168.0.119")
-# rtde_r = rtde_receive.RTDEReceiveInterface("192.168.0.119")
-# actual = rtde_r.getActualTCPPose()
-# actual[2]-=0.1
-# rtde_c.moveL(actual, 0.1, 0.3)
-# print(actual)
-# actual[2]+=0.1
-# rtde_c.moveL(actual, 0.1, 0.3)
-# print(actual)
-# rtde_c.stopScript()
 class ur_controller_node :
     def __init__(self):
         rospy.init_node("ur_controller_node")
@@ -34,7 +24,7 @@ class ur_controller_node :
     def pose_callback(self, msg):
         pose = [self.init_pose[0]+msg.position.x, self.init_pose[1]+msg.position.y, self.init_pose[2]+msg.position.z,
                 self.init_pose[3], self.init_pose[4], self.init_pose[5]]
-        self.rtde_c.moveL(pose, 0.1, 0.2)
+        self.rtde_c.moveL(pose, 0.01, 0.2)
 
 
 if __name__ == '__main__':
